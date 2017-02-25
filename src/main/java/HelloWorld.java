1fae97f40bb37f63958e85e5b626eae87f8bcf9f
deng.bowen <dh.deng.bowen@gmail.com>
2017-02-25 16:57:54 +0800

    测试提交HELLOWORLD

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by dh.deng.bowen on 2017/2/25.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
